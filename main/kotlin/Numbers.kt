824fb30ec73379a04a6bfe45bf9930101632f4be
Oleg Chumin <tschumin.oleg@gmail.com>
2023-02-28 13:05:02 +0300

    Add lesson 2

--- main/kotlin/Numbers.kt
+++ main/kotlin/Numbers.kt
@@ -29,11 +29,19 @@ fun main() {
 
 
      */
+    // 16bit = 2x8bit = 2Bytes
+    println("Short and UShort (unsigned Short) 16bit = 2x8bit = 2Bytes")
+    val shortNumMin: Short = Short.MIN_VALUE // -32_768
+    val shortNumMax: Short = Short.MAX_VALUE //  32_767
+    println("Short.MIN_VALUE = $shortNumMin; Short.MAX_VALUE = $shortNumMax;")
+    val uShortNumMin: UShort = UShort.MIN_VALUE // 0
+    val uShortNumMax: UShort = UShort.MAX_VALUE // 65_535
+    println("UShort.MIN_VALUE = $uShortNumMin; UShort.MAX_VALUE = $uShortNumMax;")
 
     //32bit = 4x8bit = 4Bytes
     println("Int and UInt (unsigned Int) 32bit = 4x8bit = 4Bytes")
     val intNumMin: Int = Int.MIN_VALUE //public const val MIN_VALUE: Int = -2_147_483_648
-    val intNumMax: Int = Int.MAX_VALUE //public const val MAX_VALUE: Int = 2_147_483_647
+    val intNumMax: Int = Int.MAX_VALUE //public const val MAX_VALUE: Int =  2_147_483_647
     println("Int.MIN_VALUE = $intNumMin; Int.MAX_VALUE = $intNumMax;")
     val uIntNumMin: UInt = UInt.MIN_VALUE // 0
     val uIntNumMax: UInt = UInt.MAX_VALUE // 4_294_967_295
@@ -42,7 +50,7 @@ fun main() {
     //64bit = 8x8bit = 64Bytes
     println("Long and ULong (unsigned Long) 64bit = 8x8bit = 64Bytes")
     val longNumMin: Long = Long.MIN_VALUE // -9_223_372_036_854_775_808
-    val longNumMax: Long = Long.MAX_VALUE // 9_223_372_036_854_775_807
+    val longNumMax: Long = Long.MAX_VALUE //  9_223_372_036_854_775_807
     println("Long.MIN_VALUE = $longNumMin; Long.MAX_VALUE = $longNumMax;")
     val uLongNumMin: ULong = ULong.MIN_VALUE // 0
     val uLongNumMax: ULong = ULong.MAX_VALUE // 18446744073709551615
