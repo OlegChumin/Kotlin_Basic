4c45fe785a453e3c7482b4a193754c6c6328da65
Oleg Chumin <tschumin.oleg@gmail.com>
2023-02-28 13:14:07 +0300

    Add lesson 2

--- main/kotlin/Numbers.kt
+++ main/kotlin/Numbers.kt
@@ -13,8 +13,6 @@ fun main() {
     println(notificationText)
     println()
 
-    // целочисленные
-    //
 
     /**
     Names of large numbers
@@ -27,8 +25,18 @@ fun main() {
     квинтиллион (30 нулей) -> quintillion (30 digits)
     квинтиллиард (33 нуля) -> quintbillion (33 digits)
 
-
      */
+    // целочисленные
+    println("Integer: Byte/UByte, Short/UShort, Int/UInt, Long/ULong")
+    // 8bit = 1Byte
+    println("Byte and UByte (unsigned Byte) 8bit = 1Byte")
+    val byteNumMin: Byte = Byte.MIN_VALUE
+    val byteNumMax: Byte = Byte.MAX_VALUE
+    println("Byte.MIN_VALUE = $byteNumMin; Byte.MAX_VALUE = $byteNumMax;")
+    val uByteNumMin: UByte = UByte.MIN_VALUE
+    val uByteNumMax: UByte = UByte.MAX_VALUE
+    println("UByte.MIN_VALUE = $uByteNumMin; UByte.MAX_VALUE = $uByteNumMax;\n")
+
     // 16bit = 2x8bit = 2Bytes
     println("Short and UShort (unsigned Short) 16bit = 2x8bit = 2Bytes")
     val shortNumMin: Short = Short.MIN_VALUE // -32_768
@@ -36,7 +44,7 @@ fun main() {
     println("Short.MIN_VALUE = $shortNumMin; Short.MAX_VALUE = $shortNumMax;")
     val uShortNumMin: UShort = UShort.MIN_VALUE // 0
     val uShortNumMax: UShort = UShort.MAX_VALUE // 65_535
-    println("UShort.MIN_VALUE = $uShortNumMin; UShort.MAX_VALUE = $uShortNumMax;")
+    println("UShort.MIN_VALUE = $uShortNumMin; UShort.MAX_VALUE = $uShortNumMax;\n")
 
     //32bit = 4x8bit = 4Bytes
     println("Int and UInt (unsigned Int) 32bit = 4x8bit = 4Bytes")
@@ -45,7 +53,7 @@ fun main() {
     println("Int.MIN_VALUE = $intNumMin; Int.MAX_VALUE = $intNumMax;")
     val uIntNumMin: UInt = UInt.MIN_VALUE // 0
     val uIntNumMax: UInt = UInt.MAX_VALUE // 4_294_967_295
-    println("UInt.MIN_VALUE = $uIntNumMin; UInt.MAX_VALUE = $uIntNumMax; \n")
+    println("UInt.MIN_VALUE = $uIntNumMin; UInt.MAX_VALUE = $uIntNumMax;\n")
 
     //64bit = 8x8bit = 64Bytes
     println("Long and ULong (unsigned Long) 64bit = 8x8bit = 64Bytes")
@@ -54,9 +62,10 @@ fun main() {
     println("Long.MIN_VALUE = $longNumMin; Long.MAX_VALUE = $longNumMax;")
     val uLongNumMin: ULong = ULong.MIN_VALUE // 0
     val uLongNumMax: ULong = ULong.MAX_VALUE // 18446744073709551615
-    println("ULong.MIN_VALUE = $uLongNumMin; ULong.MAX_VALUE = $uLongNumMax; \n")
+    println("ULong.MIN_VALUE = $uLongNumMin; ULong.MAX_VALUE = $uLongNumMax;\n")
 
-// вещественные
+    // вещественные
+
     // строковые
     // логические
 
